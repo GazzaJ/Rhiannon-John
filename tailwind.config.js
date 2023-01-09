6a0739b47a673bb58ba440735e899a4d881f1a06
GazzaJ <johnge71@gmail.com>
2023-01-09 16:40:11 +0000

    Added screen sizes and custom colours to the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    colors: {
+      'darkGray': '#37474f',
+      'pink': '#ff4f7f'
+    },
     extend: {},
   },
   plugins: [],
