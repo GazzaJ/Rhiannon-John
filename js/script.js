3408b478b0ccf6c111959221535320d2d4ea8980
GazzaJ <johnge71@gmail.com>
2023-01-12 16:22:15 +0000

    Stripped back the unneccessary code in the JS file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
-const logo = document.getElementById('logo');
 
 // Hamburger Button Listner.
 btn.addEventListener('click', navToggle);
@@ -10,10 +9,4 @@ function navToggle() {
     btn.classList.toggle('open');
     menu.classList.toggle('flex');
     menu.classList.toggle('hidden');
-
-    if (menu.classList.contains('flex')) {
-        logo.setAttribute('src', './images/logo-bookmark-footer.svg');
-    } else {
-        logo.setAttribute('src', './images/logo-bookmark.svg');
-    }
 }
