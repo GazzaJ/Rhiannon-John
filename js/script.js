3b2e25837c867c594100b04ca6df89cc76374bc8
GazzaJ <johnge71@gmail.com>
2023-01-23 18:22:25 +0000

    REFACTOR Added more JavaScript to add and style the image modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const imgs = document.querySelectorAll('.image');
-console.log(imgs);
-imgs.forEach((img) => img.addEventListener('click', openModal))
-//Display modal on clicking image
+let imgSrc;
 
-function openModal() {
-    console.log('Boom!');
-}
+// Getting the source of the image
+//Adapted from https://dev.to/salehmubashar/create-an-image-modal-with-javascript-2lf3
+imgs.forEach((img) => {
+    img.addEventListener('click', (e) => {
+        imgSrc = e.target.src;
+    });
+});
+
+//Display modal on clicking image
+let imgModal = (src) => {
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+
+    // Add the modal to the main section / parent element
+    document.querySelector(".main").append(modal);
+
+    // Add image to the modal
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", src);
+    modal.append(newImg)
+};
