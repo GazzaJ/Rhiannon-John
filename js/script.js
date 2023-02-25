cf19d1735de6ceccfed00e0a375af175cb71e006
GazzaJ <johnge71@gmail.com>
2023-02-25 15:02:15 +0000

    Add a mobile menu with interactive hamburger icon

--- js/script.js
+++ js/script.js
@@ -50,3 +50,15 @@ backToTop = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// Hamburger mobile menu button
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
